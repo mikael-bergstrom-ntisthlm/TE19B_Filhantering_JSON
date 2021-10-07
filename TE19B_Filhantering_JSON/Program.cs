37ff28da82fcf24f550d15947e3112323c31f584
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2021-10-07 13:18:44 +0200

    Initial commit

---
+++ TE19B_Filhantering_JSON/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.te
+
+
+Fighter f1 = new Fighter()
+{
+  Name = "Lars-Ove",
+  Cuddliness = 100,
+  Cuteness = 60,
+  isAPrincess = true
+};
+
+
+
+
+// string something = "Hej";
+
+// File.WriteAllText("test.txt", something);
+
+// string path = @"C:\test\";
+
+// for (int i = 0; i < 100000; i++)
+// {
+//   File.Create(path + i);
+// }
+
+// string[] files = Directory.GetFiles(path);
+
+// foreach (string file in files)
+// {
+//   Console.WriteLine(file);
+//   File.Delete(path + file);
+// }
+
+
+// if (File.Exists("test2.txt"))
+// {
+//   string thing = File.ReadAllText("test2.txt");
+//   Console.WriteLine(thing);
+// }
+
+
+Console.ReadLine();
