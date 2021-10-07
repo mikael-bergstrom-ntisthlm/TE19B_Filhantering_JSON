b2bfdfff41ff2b79dec364a64546c3eec7b27eaf
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2021-10-07 13:18:52 +0200

    adrhweyj

---
+++ TE19B_Filhantering_JSON/Fighter.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Fighter
+{
+  public string Name {get; set;}
+  public int Cuddliness {get; set;}
+  public int Cuteness {get; set;}
+  public bool isAPrincess {get; set;}
+}
